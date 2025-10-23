2690e60c9059f608a55505e877ba71d85c5ead30
m4n0jpo77al <manojpottal12@gmail.com>
2025-10-23 10:32:45 +0530

    Delete App.jsx

--- App.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import FashionLandingPage from "../landingpage"; // adjust path if you move the file
-
-export default function App() {
-  return <FashionLandingPage />;
-}
